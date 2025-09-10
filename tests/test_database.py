e5cf3b4b9ed8ab6349e06b87dd8abd7b2c395db7
Palau003 <178561@upslp.edu.mx>
2025-09-09 19:21:59 -0600

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,28 +12,40 @@ def db_connection():
     )
     yield conn
     conn.close()
-
-def test_alumnos_insertados(db_connection):
+    
+def test_carreras_insertadas(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM alumnos;")
+        cur.execute("SELECT COUNT(*) FROM carrera;")
+        count = cur.fetchone()[0]
+        assert count == 6
+
+def test_estudiantes_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM estudiante;")
+        count = cur.fetchone()[0]
+        assert count == 10
+        
+def test_profesores_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM profesor;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_maestros_insertados(db_connection):
+def test_materias_insertadas(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM maestros;")
+        cur.execute("SELECT COUNT(*) FROM materia;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_grupos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM grupos;")
+        cur.execute("SELECT COUNT(*) FROM grupo;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_inscripciones_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM inscripciones;")
+        cur.execute("SELECT COUNT(*) FROM inscripcion;")
         count = cur.fetchone()[0]
         assert count == 10  
 
@@ -42,22 +54,19 @@ def test_asistencia_insertados(db_connection):
         cur.execute("SELECT COUNT(*) FROM asistencia;")
         count = cur.fetchone()[0]
         assert count == 10  
-
+        
 def test_structure(db_connection):
     sql = ''' 
             SELECT tablename FROM pg_tables 
             WHERE tablename 
-            IN ('alumnos', 'maestros', 'grupos',
-            'inscripciones', 'asistencia');
+            IN ('carrera','estudiante', 'profesor', 'materia', 'grupo',
+            'inscripcion', 'asistencia');
           '''
-    expected_tables = {'alumnos', 'maestros',
-                       'grupos', 'inscripciones',
-                       'asistencia'}
+    expected_tables = {'carrera','estudiante', 'profesor', 'materia', 'grupo',
+            'inscripcion', 'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
         result_tables = {row[0] for row in cur.fetchall()}
         msg = f'''Expected tables {expected_tables},
               but found {result_tables}'''
         assert result_tables == expected_tables, msg
-                
-        
